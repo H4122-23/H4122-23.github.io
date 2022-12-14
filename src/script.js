06041130a3a764d51803bafa16809b701676665c
tdang2k <lamdan0008@gmail.com>
2022-12-14 11:42:26 +0100

    Implementing scientist of the day request

--- src/script.js
+++ src/script.js
@@ -17,6 +17,61 @@ LIMIT 100`
 ;
 }
 
+function findScientistOfTheDay(result_id){
+  var contenu_requete = 
+  `PREFIX owl: <http://www.w3.org/2002/07/owl#>
+  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+  PREFIX dc: <http://purl.org/dc/elements/1.1/>
+  PREFIX : <http://dbpedia.org/resource/>
+  PREFIX dbpedia2: <http://dbpedia.org/property/>
+  PREFIX dbpedia: <http://dbpedia.org/>
+  PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+  PREFIX dbo: <http://dbpedia.org/ontology/>
+  
+  SELECT DISTINCT ?name ?comment  ?birthdate  (GROUP_CONCAT( DISTINCT ?education; separator = "; ") AS ?education)  (GROUP_CONCAT( DISTINCT ?fields; separator = "; ") AS ?fields) ?homepage ?thumbnail
+  WHERE {
+    ?scientist a dbo:Scientist;
+              foaf:name ?name;
+              rdfs:comment ?comment;
+              dbo:wikiPageWikiLink ?link .
+    OPTIONAL {?scientist dbo:birthDate ?birthdate}
+    OPTIONAL {?scientist dbp:birthDate ?birthdate}
+    OPTIONAL {?scientist dbo:academicDiscipline ?fields}
+    OPTIONAL {?scientist dbp:education ?education}
+    OPTIONAL {?scientist dbp:almaMater ?education}
+    OPTIONAL {?scientist foaf:homepage ?homepage}
+    OPTIONAL {?scientist dbo:thumbnail ?thumbnail}
+    FILTER (?birthdate != "null"^^xsd:date && SUBSTR(STR(?birthdate), 6, 2) = "12" && SUBSTR(STR(?birthdate), 9, 2) = "12")
+    FILTER(langMatches(lang(?comment), "EN"))
+  }
+  ORDER BY DESC(?link)
+  LIMIT 50`;
+   // Encodage de l'URL à transmettre à DBPedia
+   var url_base = "http://dbpedia.org/sparql";
+   var url = url_base + "?query=" + encodeURIComponent(contenu_requete) + "&format=json";
+ 
+   // Requête HTTP et affichage des résultats
+   var xmlhttp = new XMLHttpRequest();
+   xmlhttp.onreadystatechange = function () {
+     if (this.readyState == 4 && this.status == 200) {
+       var results = JSON.parse(this.responseText);
+       console.log(results)
+       // Display loading spinner #loading-spinner
+     document.getElementById("loading-spinner").classList.add("d-none");
+     document.getElementById("search-icon").classList.remove("d-none");
+       afficherResultats(results, result_id);
+     }
+   };
+   xmlhttp.open("GET", url, true);
+   xmlhttp.send();
+   // Display loading spinner #loading-spinner
+   document.getElementById("loading-spinner").classList.remove("d-none");
+     document.getElementById("search-icon").classList.add("d-none");
+}
+
 function rechercher(search_word, result_id) {
   var contenu_requete = 
   `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
@@ -28,6 +83,7 @@ function rechercher(search_word, result_id) {
     FILTER(regex(?name, "${transformString(search_word)}"))
   }
   LIMIT 100`;
+ 
   // Encodage de l'URL à transmettre à DBPedia
   var url_base = "http://dbpedia.org/sparql";
   var url = url_base + "?query=" + encodeURIComponent(contenu_requete) + "&format=json";
